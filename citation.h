99fe105979412e54897f9553297acfe20cab63a9
TaxodiumPendragon <2300013210@stu.pku.edu.cn>
2024-05-03 16:21:58 +0800

    增加了基础的读取引用

--- citation.h
+++ citation.h
@@ -4,7 +4,61 @@
 
 #include <string>
 
-class Citation {
+class Citation
+{
+    std::string id;
+
+public:
+    Citation(std::string x) : id(x) {}
+    virtual void print() = 0;
+    std::string getid()
+    {
+        return id;
+    }
+};
+
+class CitBook : public Citation
+{
+    std::string isbn;
+
+public:
+    std::string author{"Pendragon"};
+    std::string title{"Hello"};
+    std::string p{"Press"};
+    int year{}; //! test
+    CitBook(std::string x, std::string i) : isbn(i), Citation(x) {}
+    void print() override
+    {
+        std::cout << "[" << getid() << "] book: " << author << ", " << title << ", " << p << ", " << year << std::endl;
+    }
+};
+class CitWeb : public Citation
+{
+    std::string url;
+
+public:
+    std::string title{"sword"}; //! test
+    CitWeb(std::string x, std::string i) : url(i), Citation(x) {}
+    void print() override
+    {
+        std::cout << "[" << getid() << "] webpage: " << title << ". Available at " << url << std::endl;
+    }
+};
+class CitArt : public Citation
+{
+    std::string title;
+    std::string author;
+    std::string journal;
+    int year;
+    int volume;
+    int issue;
+
+public:
+    CitArt(std::string x, std::string t, std::string a, std::string j, int y, int v, int i) : title(t), author(a), journal(j), year(y), volume(v), issue(i), Citation(x) {}
+    void print() override
+    {
+        std::cout << "[" << getid() << "] article: " << author << ", " << title << ", " << journal << ", " << year << ", " << volume << ", " << issue << std::endl;
+    }
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -3,26 +3,75 @@
 #include <vector>
 #include <cstring>
 
+#include "third_parties/nlohmann/json.hpp"
 #include "utils.hpp"
 #include "citation.h"
 
-//?感觉没有必要
-bool fileExits(const std::string &filename)
+std::string readFromFile(const std::string &inputPath)
 {
-    std::ifstream file(filename);
-    return file.good();
+    std::ifstream file(inputPath);
+    if (!file)
+    {
+        std::cerr << "无法打开文件: " << inputPath << std::endl;
+        std::exit(1);
+    }
+
+    std::string text;
+    std::string line;
+    while (std::getline(file, line))
+    {
+        text += line + "\n";
+    }
+
+    return text;
 }
 
 std::vector<Citation *> loadCitations(const std::string &filename)
 {
-    // FIXME: load citations from file
+    // load citations from file
+    std::vector<Citation *> citations;
+    std::ifstream file{filename};
+    if (!file.is_open())
+    {
+        std::exit(1);
+    }
+    nlohmann::json data = nlohmann::json::parse(file);
+    for (auto &cite : data["citations"])
+    {
+        Citation *citation = nullptr;
+        std::string t = cite["type"];
+        std::string id = cite["id"];
+
+        if (t == "book")
+        {
+            citation = new CitBook(id, cite["isbn"]);
+        }
+        else if (t == "webpage")
+        {
+            citation = new CitWeb(id, cite["url"]);
+        }
+        else if (t == "article")
+        {
+            int year = cite["year"].get<int>();
+            int volume = cite["volume"].get<int>();
+            int issue = cite["issue"].get<int>();
+            citation = new CitArt(id, cite["title"], cite["author"], cite["journal"], year, volume, issue);
+        }
+        if (citation != nullptr)
+        {
+            citations.push_back(citation);
+        }
+        else
+            std::exit(1);
+    }
+    return citations;
 }
 
 int main(int argc, char **argv)
 {
     bool stdo{true};
     bool stdi{true};
-    // 处理一些命令行参数错误输入情况：命令行本身参数有误
+    // 处理一些命令行参数错误输入情况，处理了命令行本身参数有误的可能情况
     if (argc != 4 && argc != 6)
         std::exit(1);
     std::string citationPath;
@@ -41,6 +90,8 @@ int main(int argc, char **argv)
             stdo = false;
             outputPath = argv[++i];
         }
+        else if (arg == "-")
+            break;
         else
         {
             // 错误的参数
@@ -50,20 +101,34 @@ int main(int argc, char **argv)
     // "docman", "-c", "citations.json", "input.txt"
     // TODO 处理错误：文章的中括号有误
     auto citations = loadCitations(citationPath);
-    std::vector<Citation *> printedCitations{};
+    std::vector<Citation *> printedCitations{citations};
 
     // FIXME: read all input to the string, and process citations in the input text
-    // auto input = readFromFile(argv[3]);
-    // ...
-
+    std::string input;
+    if (stdi)
+    {
+        std::string line;
+        while (std::getline(std::cin, line))
+        {
+            if (line.empty())
+            {
+                break;
+            }
+            input += line + "\n";
+        }
+    }
+    else
+    {
+        input = readFromFile(inputPath);
+    }
     std::ostream &output = std::cout;
-
-    // output << input;  // print the paragraph first
-    // output << "\nReferences:\n";
+    output << input; // print the paragraph first
+    output << "\nReferences:\n";
 
     for (auto c : printedCitations)
     {
-        // FIXME: print citation
+        // FIXME: 打印引用格式输出
+        c->print();
     }
 
     for (auto c : citations)
